37b4e90e811bbd609207b26e43c7a0bbc94fb22f
Bhavesh Maisuria <bhaveshmaisuria@jacksolutions.co>
2019-01-30 10:23:21 +0530

    first commit

---
+++ tests/user.assert.test.js
@@ -0,0 +1,33 @@
+import User from "../models/user.model";
+const assert = require('chai').assert;
+
+describe('Assert', () => {
+    it('should create the user', (done) => {
+        const newUserData = {
+            "username": "assert@gmail.com",
+            "password": "test"
+        };
+
+        const UserModel = new User(newUserData);
+        UserModel.save((err, data) => {
+            assert.isNull(err);
+
+            assert.isDefined(data);
+            assert.isObject(data);
+
+            assert.isDefined(data.id);
+            assert.isString(data.id);
+
+            assert.isDefined(data.username);
+            assert.isString(data.username);
+            assert.equal('assert@gmail.com', data.username);
+
+            assert.isDefined(data.password);
+            assert.isString(data.password);
+            assert.notEqual('test');
+            assert.isAbove(data.password.length, 10);
+
+            done();
+        });
+    });
+});
---
+++ tests/user.should.test.js
@@ -0,0 +1,34 @@
+import User from "../models/user.model";
+const should = require('chai').should();
+
+describe('Should', () => {
+    it('should create the user', (done) => {
+
+        const newUserData = {
+            "username": "should@gmail.com",
+            "password": "test"
+        };
+
+        const UserModel = new User(newUserData);
+        UserModel.save((err, data) => {
+            should.not.exist(err);
+
+            should.exist(data);
+            data.should.be.an('object');
+
+            should.exist(data.id);
+            data.id.should.be.a('string');
+
+            should.exist(data.username);
+            data.username.should.be.a('string');
+            data.username.should.be.equal('should@gmail.com');
+
+            should.exist(data.password);
+            data.password.should.be.a('string');
+            data.password.should.not.equal('test');
+            data.password.length.should.be.gt(10);
+
+            done();
+        });
+    });
+});
